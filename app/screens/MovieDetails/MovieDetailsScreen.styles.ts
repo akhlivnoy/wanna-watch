bae75ab29f54ef7d18f6e1d4eca1109a5cc22f38
Andrii Khlivnyi <akhlivnoy@gmail.com>
2023-06-19 12:41:18 +0300

    feat(movie-details-screen): MainHeader & poster UI/UX

--- app/screens/MovieDetails/MovieDetailsScreen.styles.ts
+++ app/screens/MovieDetails/MovieDetailsScreen.styles.ts
@@ -1,9 +1,11 @@
-import { StyleSheet, ViewStyle } from 'react-native';
+import { ImageStyle, StyleSheet, ViewStyle } from 'react-native';
 
 import { COLORS } from '#themes/colors';
 
 interface IMovieDetailsScreenStyles {
   container: ViewStyle;
+  poster: ImageStyle;
+  noImage: ViewStyle;
 }
 
 export const styles = StyleSheet.create<IMovieDetailsScreenStyles>({
@@ -13,5 +15,21 @@ export const styles = StyleSheet.create<IMovieDetailsScreenStyles>({
     paddingLeft: 16,
 
     backgroundColor: COLORS.PRIMARY[500],
+  },
+  poster: {
+    width: 170,
+    height: 250,
+
+    borderRadius: 8,
+  },
+  noImage: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 170,
+    height: 250,
+    marginRight: 16,
+
+    borderRadius: 8,
+    backgroundColor: COLORS.PRIMARY[300],
   },
 });
--- app/screens/MovieDetails/MovieDetailsScreen.tsx
+++ app/screens/MovieDetails/MovieDetailsScreen.tsx
@@ -1,13 +1,44 @@
 import React from 'react';
-import { SafeAreaView, View } from 'react-native';
+import { Image, SafeAreaView, View } from 'react-native';
 
+import { SVG } from '#assets/svg';
+import { MainHeader } from '#components';
+import { useAppSelector } from '#hooks';
+import { apiInstance } from '#services/api';
 import { generalStyles } from '#utils/styles';
 
 import { styles } from './MovieDetailsScreen.styles';
 import { MovieDetailsScreenProps } from './MovieDetailsScreen.types';
 
-export const MovieDetailsScreen: React.ComponentType<MovieDetailsScreenProps> = () => (
-  <SafeAreaView style={generalStyles.blackFlex}>
-    <View style={styles.container} />
-  </SafeAreaView>
-);
+export const MovieDetailsScreen: React.ComponentType<MovieDetailsScreenProps> = ({ navigation }) => {
+  const { movieDetails } = useAppSelector(state => state.tmdb);
+  return (
+    <SafeAreaView style={generalStyles.blackFlex}>
+      <MainHeader
+        LeftSvgIcon={SVG.LeftArrow}
+        title="Movie details"
+        onLeftIconPress={navigation.goBack}
+      />
+      <View style={styles.container}>
+        <View style={generalStyles.row}>
+          {
+            // ? Poster
+            movieDetails?.poster_path ? (
+              <Image
+                source={{ uri: apiInstance.tmdb.getImageUri(movieDetails.poster_path) }}
+                style={styles.poster}
+              />
+            ) : (
+              <View style={styles.noImage}>
+                <SVG.NoImage
+                  height={100}
+                  width={100}
+                />
+              </View>
+            )
+          }
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
